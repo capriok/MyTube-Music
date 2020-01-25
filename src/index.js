30d6d566d2160eb4e24bdc4d0f6a2e01a6fd0dc4
Kyle Caprio <capriok7@yahoo.com>
2020-01-24 17:45:25 -0700

    touches

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React, { useEffect } from 'react';
-import ReactDOM from 'react-dom';
-import { StateProvider } from './state'
-import App from './app';
-import './index.css';
-import AppTest from './component-test';
+import React, { useEffect } from "react"
+import ReactDOM from "react-dom"
+import { StateProvider } from "./state"
+import App from "./app"
+import "./index.css"
+import AppTest from "./component-test"
 
 function Index() {
   let initialState = {
     auth: {
       isAuthenticated: false,
-      token: '',
+      token: "",
       user: {}
     },
     components: {
@@ -20,50 +20,49 @@ function Index() {
       miniPlayer: true,
       fullPlayer: false,
       playlist: true,
-      playlistItems: false,
+      playlistItems: false
     },
     display: {
-      title: '',
-      id: '',
-      channelTitle: '',
+      title: "",
+      id: "",
+      channelTitle: ""
     },
     queue: [],
     playlistObj: {
-      id: '',
-      snippet: {},
+      id: "",
+      snippet: {}
     },
-    channelId: 'UC7Zyh4_j6BZEZtjnuS-PMOg',
-
+    channelId: "UC7Zyh4_j6BZEZtjnuS-PMOg"
   }
 
   const reducer = (state, action) => {
     switch (action.type) {
-      case 'manage':
+      case "manage":
         return {
           ...state,
           components: action.components
         }
-      case 'login':
+      case "login":
         return {
           ...state,
           auth: action.auth
         }
-      case 'select':
+      case "select":
         return {
           ...state,
           display: action.display
         }
-      case 'addtoq':
+      case "addtoq":
         return {
           ...state,
           queue: action.queue
         }
-      case 'cId':
+      case "cId":
         return {
           ...state,
           channelId: action.channelId
         }
-      case 'pId':
+      case "pId":
         return {
           ...state,
           playlistObj: action.playlistObj
@@ -73,45 +72,49 @@ function Index() {
     }
   }
 
-  const googleSuccess = (res) => {
+  const googleSuccess = res => {
     initialState.auth.isAuthenticated = true
     initialState.auth.token = res.Zi.access_token
     initialState.auth.user = res.w3.ig
 
-    localStorage.setItem('YTP-token', res.Zi.access_token)
-    localStorage.setItem('YTP-user', res.w3.ig)
-    window.location.href = '/'
+    localStorage.setItem("YTP-token", res.Zi.access_token)
+    localStorage.setItem("YTP-user", res.w3.ig)
+    window.location.href = "/"
   }
-  const googleFailure = (res) => {
-    console.log(res);
+  const googleFailure = res => {
+    console.log(res)
   }
 
   const logout = () => {
-    window.location.href = '/'
-    localStorage.removeItem('token')
+    window.location.href = "/"
+    localStorage.removeItem("token")
   }
 
   useEffect(() => {
-    let authorize = localStorage.getItem('YTP-token')
+    let authorize = localStorage.getItem("YTP-token")
     if (authorize) {
       initialState.auth.isAuthenticated = true
-      console.log('Welcome to YT Player')
-      console.log('Auth Status ->', initialState.auth.isAuthenticated);
-      console.log('channelFetchId ->', initialState.channelId);
+      console.log("Welcome to YT Player")
+      console.log("Auth Status ->", initialState.auth.isAuthenticated)
+      console.log("channelFetchId ->", initialState.channelId)
     }
     if (!initialState.display.id) {
       initialState.components.fullPlayer = false
     }
-  }, [initialState.auth.isAuthenticated, initialState.channelId,])
+  }, [initialState.auth.isAuthenticated, initialState.channelId])
 
   return (
     <StateProvider initialState={initialState} reducer={reducer}>
-      <AppTest />
-      <App googleSuccess={googleSuccess} googleFailure={googleFailure} logout={logout} />
+      {/* <AppTest /> */}
+      <App
+        googleSuccess={googleSuccess}
+        googleFailure={googleFailure}
+        logout={logout}
+      />
     </StateProvider>
   )
 }
 
-ReactDOM.render(<Index />, document.getElementById('root'));
+ReactDOM.render(<Index />, document.getElementById("root"))
 
 // ReactDOM.render(<Index />, document.getElementById('root'));
