f6e66e20229b3105e0101466dc768a161213071b
Kyle Caprio <capriok7@yahoo.com>
2020-02-03 20:02:24 -0700

    touches

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useStateValue } from '.././state'
+import { Transition } from 'react-spring/renderprops'
 import logo from '../img/logo.png'
 import search from '../img/search.png'
 import logout from "../img/logout.png";
@@ -16,25 +17,38 @@ export default function Navbar({ initialComponentState }) {
    }
 
    return (
-      <nav>
-         <img className="nav-profile" alt=""
-            src={user.avatar}
-            onClick={() => openBin(!bin)}
-         />
-         {bin &&
-            <div className="logout-bin">
-               <div className="logout-icon">
-                  <img src={logout} alt="" onClick={handleLogout} />
+      <>
+         <Transition
+            items={bin}
+            from={{ position: 'absolute', left: 5, top: '0px', }}
+            enter={{ position: 'absolute', left: 5, top: '50px', }}
+            leave={{ position: 'absolute', left: 5, top: '0px', }}
+
+         >
+            {results => bin && (props => (
+               <div style={props}>
+                  <div className="logout-bin">
+                     <div className="logout-icon">
+                        <img src={logout} alt="" onClick={handleLogout} />
+                     </div>
+                  </div>
                </div>
+            ))}
+         </Transition>
+         <nav>
+            <img className="nav-profile" alt=""
+               src={user.avatar}
+               onClick={() => openBin(!bin)}
+            />
+
+            <div onClick={initialComponentState}>
+               <img className="nav-logo" src={logo} alt="" />
             </div>
-         }
-         <div onClick={initialComponentState}>
-            <img className="nav-logo" src={logo} alt="" />
-         </div>
-         <div className="nav-buttons">
-            <img className="nav-button" src={search} alt=""
-               onClick={() => dispatch({ type: 'manage', components: { ...components, search: !components.search } })} />
-         </div>
-      </nav>
+            <div className="nav-buttons">
+               <img className="nav-button" src={search} alt=""
+                  onClick={() => dispatch({ type: 'manage', components: { ...components, search: !components.search } })} />
+            </div>
+         </nav >
+      </>
    )
 }
